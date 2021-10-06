d2f7c9d663c6435733f88b1dea73873afa4d3ed0
Bradley Ellis <bradleyaellis@gmail.com>
2021-10-05 22:40:12 -0600

    Add pug and axios. add temporary login page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,26 @@
-<template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+<template lang="pug">
+  v-app
+    v-main
+      div
+        v-text-field(v-model="email" label="E-Mail")
+        v-text-field(type="password" label="Password" v-model="password")
+        v-btn(@click="login") Login
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from "axios"
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
+    password: null,
+    email: null
     //
   }),
+  methods: {
+    login(){
+      axios.post("localhost:3000/sessions/create", { email: this.email, password: this.password }).then().catch(err => console.log(err))
+    }
+  }
 };
 </script>
